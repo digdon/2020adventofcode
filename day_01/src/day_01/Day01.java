282275550da64f19f9e3ee23144cda6cd1abe7aa
Mike Digdon <Mike.Digdon@viacomcontractor.com>
2020-12-03 18:44:42 -0400

    Removing an unused import.

--- day_01/src/day_01/Day01.java
+++ day_01/src/day_01/Day01.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Day01 {
 
